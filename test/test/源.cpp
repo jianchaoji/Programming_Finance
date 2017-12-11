7704f775b2e13e3b92863184c2b4706d83c161af
Jianchao_Ji <jianchaoji@gmail.com>
2017-12-11 11:56:56 -0500

    Programming_Finance

---
+++ test/test/源.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	int i = 1;
+	if (i == 1)
+	{
+		i++;
+		cout << "the first" << endl;
+	}
+	else 
+	{
+		cout << "the second" << endl;
+	}
+
+
+	system("pause");
+}
